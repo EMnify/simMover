d0a108423cc9309a45c9bb08e20c51a19f461780
Michael Streiner <michael.streiner@emnify.com>
2018-11-23 17:20:39 +0100

    implemented releasing of sims from endpoints before moving them

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ This script is here to show how you can build custom logic on top of our compreh
 
 It will help you shift a batch of SIM cards from one organisation to another one in case you need to.
 
-It is build in the programming language Node.js (Java Script) which is an easy language most developers and even frontend developers can develop and that runs on all platforms.
+It is build in the programming language Node.js (Java Script) which is an easy language most developers can develop and that runs on all platforms.
 
 The execution of the API requests is being throttled to 2 requests per second in order to not get blocked due to overloading the API.
+
+The account you use for executin needs to have the "support" role assigned, otherwise the script can't detatch the SIMs from the endpoints of the organisation they are moved away from.
 
 ## Installation
 ### On Windows 
@@ -36,12 +38,21 @@ node index.js --help
 ## Usage
 > The script can take a file as input like the sample file with comma seperated IMSIS like so:
 ```
-node index.js --imsiCsvFile sampleImsis.csv --destinationOrgId 4192 --dryRun --appToken yourApplicationToken
+node index.js --imsiCsvFile sampleImsis.csv --destinationOrgId 4192 --dryRun --appToken tokenOfYouAccount --enterpriseAppToken appTokenOfTheEnterpriseWithTheSimsCurrentlyAssigned
 ```
 > Or you give the imsis directly as parameters like so:
 ```
-node index.js --imsiList 123456789123456,223456789123456 --destinationOrgId 4192 --dryRun --appToken yourApplicationToken
+node index.js --imsiList 123456789123456,223456789123456 --destinationOrgId 4192 --dryRun --appToken tokenOfYouAccount --enterpriseAppToken appTokenOfTheEnterpriseWithTheSimsCurrentlyAssigned
 ```
-> Of course the destination organisation ID as well as an app token for authentication need to be provided.
 
->The --dryRun parameter is optional and will skipp the actual updating of the SIMs if set.
+### Parameters
+| Shorthand | Parameter     | Description                                                                                                | Required | Sample                          |
+|-----------|---------------|------------------------------------------------------------------------------------------------------------|----------|---------------------------------|
+| -V        | --version     | output the version number                                                                                  | false    |                                 |
+| -i        | --imsiList    | IMSIs to be moved                                                                                          |          | 123456789123456,223456789123456 |
+| -f        | --imsiCsvFile | Path to a file that contains a comma seperated list of IMSIs Reuqired format: UTF-8 encoding & NO headline |          | sample.csv                      |
+| -o | --destinationOrgId   | Destination organisation ID to move them to                                     | true  | 1234  |
+| -d | --dryRun             | Outputs planned changes without executing them live                             | false |       |
+| -t | --appToken           | Application token of the account you act from                                   | true  | token |
+| -e | --enterpriseAppToken | Application token of the enterprise account you want to move the SIMs away from | true  | token |
+| -h | --help               | Outputs usage information                                                       | false |       |
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request');
 const throttledRequest = require('throttled-request')(request);
 const fs = require('fs');
 const path = require('path');
+const events = require('events');
 
 throttledRequest.configure({
   requests: 2,
@@ -17,32 +18,145 @@ program
   .option('-i, --imsiList [list of imsis]', 'IMSIs to be moved like 123456789123456,223456789123456')
   .option('-f, --imsiCsvFile [path]', 'Path to a file that contains a comma seperated list of IMSIs in UTF-8 encoding without a headline')
   .option('-o, --destinationOrgId [orgId]', 'Destination organisation ID to move them to')
-  .option('-d, --dryRun', 'Output changes without applying executing them live')
-  .option('-t, --appToken [token]', 'Application token for authorisation')
+  .option('-d, --dryRun', 'Output changes without executing them live')
+  .option('-t, --appToken [token]', 'Application token of the account you act from')
+  .option('-e, --enterpriseAppToken [token]', 'Application token of the enterprise account you want to move the SIMs away from')
   .parse(process.argv);
 
-const API_URL = "https://cdn.emnify.net/api/v1";
-const APP_TOKEN = program.appToken;
-let auth_token;
+const eventEmitter = new events.EventEmitter();
 
-if (APP_TOKEN) {
-  request.post(API_URL + "/authenticate", {
-    body: {
-      "application_token": APP_TOKEN
-    },
-    json: true
-  }, function (err, res, body) {
-    if (err) {
-      return console.error("Error authenticating with the application token", err);
-    }
-    if (res.statusCode === 200) {
-      console.log("Successfully authenticated using the application token");
-      auth_token = body.auth_token;
-      readImsis();
-    } else {
-      return console.error("Errorcode", res.statusCode, "occured while authenticating");
-    }
+const API_URL = "https://cdn.emnify.net/api/v1";
+let masterToken;
+let enterpriseToken;
+let imsis;
+let arrayOfSimIds;
+getAuthToken(program.appToken, "master");
+getAuthToken(program.enterpriseAppToken, "enterprise");
+
+
+eventEmitter.on("enterprise authentication success", function(token) {
+  enterpriseToken = token;
+});
+
+eventEmitter.on("master authentication success", function(token) {
+  masterToken = token;
+  imsis = readImsis();
+  getSimIdsFromImsis(imsis);
+});
+
+eventEmitter.on("simids pulled", function(simIds) {
+  arrayOfSimIds = simIds;
+  searchForEndpointsBySimId(arrayOfSimIds)
+});
+
+eventEmitter.on("endpoints pulled", function (endpointIds) {
+  releaseSimsFromEndpoints(endpointIds);
+});
+
+eventEmitter.on("sims released from endpoints", function () {
+  updateAllSimsOrgId(arrayOfSimIds);
+});
+
+function searchForEndpointsBySimId(simIds) {
+let simsProcessed = 0;
+(function () {
+  let arrayOfEndpointIds = [];
+
+  simIds.forEach(function (simId, index, array) {
+    throttledRequest(API_URL + "/endpoint?page=1&per_page=2&q=sim:" + simId, {
+      'auth': {
+        'bearer': masterToken
+      },
+      json: true
+    }, function (err, res, body) {
+      if (err) {
+        return console.error("Error getting the endpoint for simId", simid, err);
+      }
+      else if (!body.length) {
+        console.log("SIM", simId, "is not connected to an endpoint");
+      }
+      else if (body.length > 1) {
+        return console.error("SIM", simId, "matches more than one SIM.");
+      }
+      else if (res.statusCode === 200) {
+        console.log(body);
+        let endpointId = body[0].id;
+        arrayOfEndpointIds.push(endpointId);
+        console.log('SIM', simId, 'is connected to', endpointId);
+        simsProcessed++;
+        if (simsProcessed === array.length) {
+          eventEmitter.emit("endpoints pulled", arrayOfEndpointIds);
+          return arrayOfEndpointIds;
+        }
+      } else {
+        return console.error("Errorcode", res.statusCode, "occured while getting endpoint for SIM", simId);
+      }
+    });
   });
+})();
+}
+
+function releaseSimsFromEndpoints(endpointIds) {
+    let endpointsProcessed = 0;
+    endpointIds.forEach(function (endpointId, index, array) {
+      if (program.dryRun) {
+        console.log('DRY RUN: Release sim from endpoint', endpointId);
+      } else {
+        throttledRequest({
+          method: 'PATCH',
+          uri: API_URL + "/endpoint/" + endpointId,
+          auth: {
+            bearer: enterpriseToken
+          },
+          body: {
+            sim: {
+              id: null
+            }
+          },
+          json: true
+        }, function (err, res, body) {
+          if (err) {
+            return console.error("Error releasing the SIM for endpoint", endpointId, err);
+          }
+          else if (res.statusCode === 204) {
+            console.log('Released sim from endpoint', endpointId);
+            endpointsProcessed++;
+            if (endpointsProcessed === array.length) {
+              eventEmitter.emit("sims released from endpoints");
+              return true;
+            }
+          } else {
+            return console.error("Errorcode", res.statusCode, "occured while updating endpoint", endpointId);
+          }
+        });
+      }
+    });
+}
+
+function getAuthToken(token, orgType) {
+  if (!token) {
+    console.log("Missing a token, try executing --help to see how this script should be used");
+    return false
+  } else {
+    request.post(API_URL + "/authenticate", {
+      body: {
+        "application_token": token
+      },
+      json: true
+    }, function (err, res, body) {
+      if (err) {
+        return console.error("Error authenticating with the application token", err);
+      }
+      if (res.statusCode === 200) {
+        console.log("Successfully authenticated using the application token");
+        eventEmitter.emit(orgType + " authentication success", body.auth_token);
+        return body.auth_token;
+      }
+      else {
+        return console.error("Errorcode", res.statusCode, "occured while authenticating");
+      }
+    });
+  }
 }
 
 function readImsis() {
@@ -55,14 +169,18 @@ function readImsis() {
       if (!err) {
         imsis = csvContent.split(',');
         console.log("Sucessfully read the CSV file with the content", imsis);
-        getSimIdsFromImsis(imsis);
+        return imsis;
       } else {
         console.log(err);
       }
     });
   } else if (program.imsiList) {
     imsis = program.imsiList.split(',');
-    getSimIdsFromImsis(imsis);
+    console.log("Sucessfully read the imsis from the CLI", imsis);
+    return imsis;
+  }
+  else {
+    return console.error("Missing IMSIs to be moved, please specify a file or the list directly in the CLI");
   }
 }
 
@@ -71,29 +189,29 @@ function getSimIdsFromImsis(imsis) {
   (function () {
     let arrayOfSimIds = [];
     imsis.forEach(function (imsi, index, array) {
-      //TODO query EMnify API for SIM ID by ICCID
       throttledRequest(API_URL + "/sim?page=1&per_page=2&q=imsi:" + imsi, {
         'auth': {
-          'bearer': auth_token
+          'bearer': masterToken
         },
         json: true
       }, function (err, res, body) {
         if (err) {
-          return console.error("Error getting the SIM for", imsi, err);
+          return console.error("Error getting the SIM for IMSI", imsi, err);
         }
-        if (!body.length) {
+        else if (!body.length) {
           return console.error("IMSI", imsi, "matches no SIM.");
         }
-        if (body.length > 1) {
+        else if (body.length > 1) {
           return console.error("IMSI", imsi, "matches more than one SIM.");
         }
-        if (res.statusCode === 200) {
+        else if (res.statusCode === 200) {
           let simId = body[0].id;
           arrayOfSimIds.push(simId);
-          console.log('sim ID for', imsi, 'is', simId);
+          console.log('SIM ID for IMSI', imsi, 'is', simId);
           imsiProcessed++;
           if (imsiProcessed === array.length) {
-            updateAllSimsOrgId(arrayOfSimIds);
+            eventEmitter.emit("simids pulled", arrayOfSimIds);
+            return arrayOfSimIds;
           }
         }
         else {
@@ -115,7 +233,7 @@ function updateAllSimsOrgId(simIds) {
         method: 'PATCH',
         uri: API_URL + "/sim/" + simId,
         auth: {
-          bearer: auth_token
+          bearer: masterToken
         },
         body: {
           customer_org: {
@@ -127,7 +245,7 @@ function updateAllSimsOrgId(simIds) {
         if (err) {
           return console.error("Error patching the SIM for simId", simId, err);
         }
-        if (res.statusCode === 200) {
+        else if (res.statusCode === 200) {
           console.log('Update simId', simId, 'to organisation', program.destinationOrgId);
           simsProcessed++;
           if (simsProcessed === array.length) {
